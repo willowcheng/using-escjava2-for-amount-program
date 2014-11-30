91d6240858c922ad4111ced71f9596eb056e3d8e
Willow Cheng <e.wingus@Gmail.com>
2014-11-30 18:21:30 -0500

    Final revision

--- Amount.java
+++ Amount.java
@@ -2,7 +2,7 @@ public class Amount{
 
  private int cents;
  //@ invariant cents < 100 && cents > -100;
- 
+
  private int dollars;
  //@ invariant dollars > 0 ==> cents >= 0;
  //@ invariant dollars < 0 ==> cents <= 0;
@@ -14,14 +14,17 @@ public class Amount{
    this.dollars = dollars;
    this.cents = cents;
  }
+
  //@ ensures \result != null;
  public Amount negate(){
    return new Amount(-this.dollars, -this.cents);
  }
+
  //@ requires a != null;
  public Amount subtract(Amount a){
    return this.add(a.negate());
  }
+
  //@ requires a != null;
  public Amount add(Amount a){
    int new_dollars = dollars + a.dollars;
@@ -29,22 +32,22 @@ public class Amount{
    if (new_cents < -100) {
       new_cents = new_cents + 100;
       new_dollars = new_dollars - 1;
-      return new Amount(new_dollars,new_cents);
+      return new Amount(new_dollars, new_cents);
    }
    else if (new_cents > 100) {
       new_cents = new_cents - 100;
-      new_dollars = new_dollars - 1;
-      return new Amount(new_dollars,new_cents);
+      new_dollars = new_dollars + 1;
+      return new Amount(new_dollars, new_cents);
    }
    else if (new_cents < 0 && new_dollars > 0) {
        new_cents = new_cents + 100;
        new_dollars = new_dollars - 1;
-       return new Amount(new_dollars,new_cents);
+       return new Amount(new_dollars, new_cents);
    }
    else if (new_cents > 0 && new_dollars < 0) {
        new_cents = new_cents - 100;
        new_dollars = new_dollars + 1;
-       return new Amount(new_dollars,new_cents);
+       return new Amount(new_dollars, new_cents);
    }
  }
 
